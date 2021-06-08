8f21deff6e00656fdfd488da5e47162b48bc5b9a
offcn-java <aaa@bbb>
2021-06-08 23:09:52 +0800

    Initial commit

---
+++ src/com/test/Demo01.java
@@ -0,0 +1,21 @@
+package com.test;
+
+/**
+ * @Auther: sqy
+ * @Date: 2021/6/8 22:44
+ * @Description:
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+
+    public static void print(){
+        System.out.println("哈哈");
+    }
+
+    public static void print2(){
+        System.out.println("哈哈2");
+    }
+}
